c5e17b45c687ea64613cf50262826a4ba783469a
uscclarkey <usc_clarkey@hotmail.com>
2014-04-09 01:27:33 +0200

    changed url

--- plugin.video.clarkeystreams/default.py
+++ plugin.video.clarkeystreams/default.py
@@ -227,7 +227,7 @@ def get_xml_database(url, browse=False):
                                 addDir(name,url+href,11,icon,fanart,'','','','','download')
 
 def getclarkey(browse=False):
-        url = 'http://pastebin.com/raw.php?i=S5MAspxk'
+        url = 'https://raw.githubusercontent.com/uscclarkey/repo/master/Clarkey%20Streams/'
         soup = BeautifulSoup(makeRequest(url), convertEntities=BeautifulSoup.HTML_ENTITIES)
         files = soup('ul')[0]('li')[1:]
         for i in files:
